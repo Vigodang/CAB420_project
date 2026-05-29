6b9c225fea790e0b42315882e239abae77d48048
J-O-9000 <olsejami@gmail.com>
2026-05-29 11:50:34 +1000

    Update models_keras.py

--- libs/models_keras.py
+++ libs/models_keras.py
@@ -1,11 +1,12 @@
 from libs import models_UNet
+from keras import layers, mixed_precision
+mixed_precision.set_global_policy('mixed_float16')
 
-def build_unet(size=300, basef=64, maxf=512, encoder='resnet50', pretrained=True, image_channels=3, use_elevation=True):
+def build_unet(size=128, basef=64, maxf=512, encoder='resnet50', pretrained=True, image_channels=3, use_elevation=True, activation=layers.ReLU):
     return models_UNet.build(
         size=size,
         basef=basef,
         maxf=maxf,
-        encoder=encoder,
-        pretrained=pretrained, 
+        activation=activation,
         image_channels=image_channels, 
         use_elevation=use_elevation)
